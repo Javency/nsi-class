481d4ac01e97c71e55e35d01cc4dac7d6d56de21
javency <635142482@qq.com>
2018-07-19 18:02:58 +0800

    更换mac价格 添加百度统计

---
+++ admin/activity/js/visitTotal.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?b47523672ea7966981f87de0f8661ca4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
