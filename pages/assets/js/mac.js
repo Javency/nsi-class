bbeaba23452937461358bd5936772f5a571ab74b
javency <635142482@qq.com>
2018-08-24 18:03:28 +0800

    完善作业提交功能

--- pages/assets/js/mac.js
+++ pages/assets/js/mac.js
@@ -5,7 +5,9 @@ $(function() {
         doHomework = $("#doHomework"),
         cancle = $("#cancle"),
         confirm = $("#confirm"),
-        stuAnswer = $("#stuAnswer")
+        stuAnswer = $("#stuAnswer"),
+        // 课程Id
+        courseId = 10001
 
     $.ajax({
         method: "get",
@@ -14,15 +16,21 @@ $(function() {
             // console.log(msg.data)
             tabCourseList.html('')
             var moreTemplate = '<li class="text-center moreBox" id="more"><span class="iconfont icon-shuangjiantou more"></span></li>'
-            for (var i = 5; i < msg.data.length; i++) {
+            for (var i = 0; i < 5; i++) {
                 var template = '<li>' +
-                    '<a href="javascript:;" class="tabCurrent">' + (i + 1) + " " + msg.data[i].CourseName + '</a>' +
+                    '<a href="javascript:;" class="tabCurrent" data-courseId=' + msg.data[i].Id + '>' + (i + 1) + " " + msg.data[i].CourseName + '</a>' +
                     '</li>'
                 tabCourseList.append(template)
             }
             tabCourseList.append(moreTemplate)
 
-            $("#more").click(function() {
+            // 点击课程更换教师作业
+            changeTeaHomework()
+
+            // 点击课程更换作业列表
+            changeHomeWorkList()
+
+            $("#more").unbind('click').click(function() {
                 $("#more").hide(0)
                 tabCourseList.css("height", "auto")
                 $.ajax({
@@ -33,10 +41,13 @@ $(function() {
                         tabCourseList.html('')
                         for (var i = 0; i < msg.data.length; i++) {
                             var template = '<li>' +
-                                '<a href="javascript:;" class="tabCurrent">' + (i + 1) + " " + msg.data[i].CourseName + '</a>' +
+                                '<a href="javascript:;" class="tabCurrent" data-courseId=' + msg.data[i].Id + '>' + (i + 1) + " " + msg.data[i].CourseName + '</a>' +
                                 '</li>'
                             tabCourseList.append(template)
                         }
+                        // 获取教师问题及学生作业列表
+                        changeTeaHomework()
+                        changeHomeWorkList()
                     }
                 })
             })
@@ -106,11 +117,12 @@ $(function() {
         }
     }
 
+
+
     // 提交至后台
     function sendUserWork() {
-        var userMail = $.cookie('username'),
-            courseId = 10001,
-            UserHomeWork = editor.txt.html(),
+        var userMail = $.cookie('username')
+        UserHomeWork = editor.txt.html(),
             data = {
                 'usermail': userMail,
                 'courseId': courseId,
@@ -152,71 +164,165 @@ $(function() {
         }
     }
 
+    // 点击更换教师作业
+    function changeTeaHomework() {
+        $(".tabCurrent").on('click', function() {
+            courseId = $(this).attr("data-courseId")
+            $.ajax({
+                method: "post",
+                url: changeUrl.address + "/assignment/get_teacher_assignment.do",
+                data: {
+                    'courseId': $(this).attr("data-courseId")
+                },
+                success: function(msg) {
+                    $(".teaQuestion").text(msg.data.assignmentContent)
+                    $(".teaName").text(msg.data.userName)
+                    $(".teaQueTime").text(formatDate(msg.data.createTime))
+
+                    console.log(msg)
+                }
+            })
+        })
+    }
+
+    // 点击更换学生作业列表
+    function changeHomeWorkList() {
+        $(".tabCurrent").on('click', function() {
+            courseId = $(this).attr("data-courseId")
+            $.ajax({
+                method: "post",
+                url: changeUrl.address + "/assignment/get_student_assignment.do",
+                data: {
+                    'courseId': $(this).attr("data-courseId")
+                },
+                success: function(msg) {
+                    getStuHomework()
+                }
+            })
+        })
+    }
+
     // 获取学生作业列表
-    // $.ajax({
-    //     method: "post",
-    //     url: changeUrl.address + "/assignment/get_student_assignment.do",
-    //     data: {
-    //         'courseId': 10001
-    //     },
-    //     success: function(msg) {
-    //         stuAnswer.html("")
-    //         console.log(msg.data)
-    //         var _length = msg.data.length
-    //         $(".personNum").text(_length)
-    //         for (var i = 0; i < _length; i++) {
-    //             var template =
-    //                 '<div class="col-md-12 pt15">' +
-    //                 '<div class="row bottomLine">' +
-    //                 '<div class="col-md-1">' +
-    //                 '<img src="../images/userPic/eg08.png" alt="" width="40">' +
-    //                 '</div>' +
-    //                 '<div class="col-md-11">' +
-    //                 '<p><span class="stuName">' + msg.data[i].userName + '</span></p>' +
-    //                 '<p class="stuAnswerContent">' + msg.data[i].assignmentContent + '</p>' +
-    //                 '<p class="text-right stuAnswerTime">' + formatDate(msg.data[i].createTime) + '</p>' +
-    //                 '</div>' +
-    //                 '</div>' +
-    //                 '</div>'
-    //             stuAnswer.append(template)
-    //         }
-    //     }
-    // })
+    function getStuHomework() {
+        $.ajax({
+            method: "post",
+            url: changeUrl.address + "/assignment/get_student_assignment.do",
+            data: {
+                'courseId': courseId
+            },
+            success: function(msg) {
+                stuAnswer.html("")
+                    // console.log(msg.data)
+                var _length = msg.data.length
+                $(".personNum").text(_length)
+                for (var i = 0; i < _length; i++) {
+                    var template = '<div class="col-md-12 pt15 answerItem" data-uid=' + msg.data[i].id + '>' +
+                        '<div class="row bottomLine">' +
+                        '<div class="col-md-1">' +
+                        '<img src="../images/userPic/eg08.png" alt="" width="40">' +
+                        '</div>' +
+                        '<div class="col-md-11">' +
+                        '<p><span class="stuName">' + msg.data[i].userName + '</span></p>' +
+                        '<p class="stuAnswerContent">' + msg.data[i].assignmentContent + '</p>' +
+                        '<p class="text-right stuAnswerTime"><span class="iconfont icon-huifu reply"></span>' + formatDate(msg.data[i].createTime) + '</p>' +
+                        '<div class="replyBox">' +
+                        '<div class="replayInputBox">' +
+                        '<img src="../images/userPic/eg05.png" alt="" width="35" class="replyPic">' +
+                        '<input type="text" class="txt">' +
+                        '<a href="javascript:;" class="replyBtn" data-sendId=' + msg.data[i].id + '>回复</a>' +
+                        '<span class="iconfont icon-guanbi closeBtn"></span>' +
+                        '</div>' +
+                        '<div class="replayList">' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                    stuAnswer.append(template)
+                }
 
-    // 回复作业
-    var reply = $("#reply"),
-        closeBtn = $("#closeBtn"),
-        replayInputBox = $("#replayInputBox"),
-        replayList = $("#replayList")
+                // 回复作业
+                var reply = $(".reply"),
+                    closeBtn = $(".closeBtn"),
+                    replyBtn = $(".replyBtn"),
+                    txt = $(".txt"),
+                    replayInputBox = $(".replayInputBox")
 
-    reply.click(function() {
-        replayInputBox.show(200)
-    })
-    closeBtn.click(function() {
-        replayInputBox.hide(200)
-    })
+                reply.each(function(i, e) {
+                    $(this).on('click', function() {
+                        replayInputBox.eq(i).show(200)
+                    })
+                })
 
-    $.ajax({
-        method: "get",
-        url: changeUrl.address + "/Comment/list.do",
-        data: {
-            CourseId: 10001
-        },
-        success: function(msg) {
-            replayList.html("")
-            console.log(msg.data)
-            for (var j = 0; j < msg.data.length; j++) {
-                var template = '<div class="row mb15">' +
-                    '<div class="col-md-1">' +
-                    '<img src="../images/userPic/eg05.png" alt="" width="35" class="replyPic">' +
-                    '</div>' +
-                    '<div class="col-md-11 pl0">' +
-                    '<p class="userReplayName">' + msg.data[j].commentatorname + '</p>' +
-                    '<p class="userReplayContent">' + msg.data[j].content + '</p>' +
-                    '</div>' +
-                    '</div>'
-                replayList.append(template)
+                closeBtn.each(function(i, e) {
+                    $(this).on('click', function() {
+                        // console.log(i) unbind('click')
+                        replayInputBox.eq(i).hide(200)
+                    })
+                })
+
+                replyBtn.each(function(i, e) {
+                    $(this).on('click', function() {
+                        var userVal = txt.eq(i).val()
+                        var objId = $(this).attr("data-sendId")
+                        $.ajax({
+                            method: "post",
+                            data: {
+                                objectid: objId,
+                                content: userVal,
+                                commentatormail: $.cookie('username'),
+                                commentatorname: $.cookie('User_TureName'),
+                                classify: "在线课堂",
+                                thumbsUp: 0,
+                                verifysign: 0,
+                                identity: 0
+                            },
+                            url: "http://192.168.0.8:8080/nsi-1.0/Comment/add.do",
+                            success: function(msg) {
+                                console.log(msg)
+                                layer.msg("评论成功，审核通过后您的回复将展示在此")
+                            }
+                        })
+                    })
+                })
+
+                // 获取作业评论列表
+                var uidList = []
+                $(".answerItem").each(function(i, e) {
+                    uidList.push($(this).attr("data-uid"))
+                })
+
+                // console.log(replayList)
+                for (let k = 0; k < uidList.length; k++) {
+                    $.ajax({
+                        method: "get",
+                        url: "http://192.168.0.8:8080/nsi-1.0/Comment/list.do",
+                        data: {
+                            CourseId: uidList[k]
+                        },
+                        success: function(msg) {
+                            // $(".replayList").html("")
+                            // $(".replayList").append("123")
+                            if (msg.data.length > 0) {
+                                for (let j = 0; j < msg.data.length; j++) {
+                                    var template = '<div class="row mb15">' +
+                                        '<div class="col-md-1">' +
+                                        '<img src="../images/userPic/eg05.png" alt="" width="35" class="replyPic">' +
+                                        '</div>' +
+                                        '<div class="col-md-11 pl0">' +
+                                        '<p class="userReplayName">' + msg.data[j].commentatorname + '</p>' +
+                                        '<p class="userReplayContent">' + msg.data[j].content + '</p>' +
+                                        '</div>' +
+                                        '</div>'
+                                    $(".replayList").eq(k).append(template)
+                                }
+                            }
+                        }
+                    })
+                }
             }
-        }
-    })
+        })
+    }
+
+    getStuHomework()
 })
